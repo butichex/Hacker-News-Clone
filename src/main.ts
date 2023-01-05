b034cfdbfeb4d2bfab902bb239a2efc24cca3457
Тимур Дютин <tdyutin@gmail.com>
2023-01-05 11:18:31 +0300

    chore: добавить нормалайзер CSS, библиотеку иконок

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import 'reset-css'
+import 'primeicons/primeicons.css';
+
+
 
 
 createApp(App).use(store).use(router).mount('#app');
